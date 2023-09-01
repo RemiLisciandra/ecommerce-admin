a322b9c9f7a604cb16c61199411825a9d36fd444
RemiLisciandra <rlisciandra@gmail.com>
2023-09-01 23:44:44 +0200

    Settings page part 2

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -30,8 +30,8 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({params}) => {
 
     return (
         <div className="flex-col">
-            <div className="flex space-y-4 p-8 pt-6">
-                <SettingsForm initialData={store}/>
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store} />
             </div>
         </div>
     )
